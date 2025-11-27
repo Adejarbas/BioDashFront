647cf7f1170ed478260dc667ccb83a3d9c74d829
Thiago <thiago.neves7@fatec.sp.gov.br>
2025-11-27 01:03:52 -0300

    Inclusão HOTJAR para mapa de calor

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next"
 import { GeistSans } from "geist/font/sans"
 import { GeistMono } from "geist/font/mono"
 import "./globals.css"
+import Script from "next/script"
 import SupportModal from "@/components/support-modal"
 
 export const metadata: Metadata = {
@@ -26,6 +27,17 @@ html {
   --font-mono: ${GeistMono.variable};
 }
         `}</style>
+        {/* Hotjar Tracking Code for Perícia Digital */}
+        <Script id="hotjar-init" strategy="afterInteractive">
+          {`(function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:6420222,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
+        </Script>
       </head>
       <body>
         {children}
