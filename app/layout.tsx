c6903564945b8582b3822a15f14bd28c3fe19df0
Thiago <thiago.neves7@fatec.sp.gov.br>
2025-11-27 01:35:45 -0300

    Correção código HOTJAR

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,11 +27,11 @@ html {
   --font-mono: ${GeistMono.variable};
 }
         `}</style>
-        {/* Hotjar Tracking Code for Perícia Digital */}
+        {/* Hotjar Tracking Code for https://bio-dash-front-theta.vercel.app */}
         <Script id="hotjar-init" strategy="afterInteractive">
           {`(function(h,o,t,j,a,r){
             h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-            h._hjSettings={hjid:6420222,hjsv:6};
+            h._hjSettings={hjid:6587304,hjsv:6};
             a=o.getElementsByTagName('head')[0];
             r=o.createElement('script');r.async=1;
             r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
